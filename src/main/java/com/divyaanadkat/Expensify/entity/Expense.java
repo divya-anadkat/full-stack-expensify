d4d8fdd6cb6d24eaa46361e85426994fe2b77af2
divya-anadkat <div.anadkat@gmail.com>
2025-01-12 21:31:30 +0530

    feat: Added Builder Pattern for Expense & Status

--- src/main/java/com/divyaanadkat/Expensify/entity/Expense.java
+++ src/main/java/com/divyaanadkat/Expensify/entity/Expense.java
@@ -10,15 +10,55 @@ public class Expense {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String merchant;
-
     private String description;
-
     private LocalDate purchaseDate;
-
     private double amount;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Status status;
+
+    // For Hibernate
+    private Expense() {}
+
+    // For Builder
+    private Expense(Builder builder){
+        this.merchant = builder.merchant;
+        this.description = builder.description;
+        this.purchaseDate = builder.purchaseDate;
+        this.amount = builder.amount;
+        this.status = Status.builder().state(Status.State.IN_REVIEW).build();
+    }
+
+    // Expose Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String merchant;
+        private String description;
+        private LocalDate purchaseDate;
+        private double amount;
+
+        public Builder merchant(String merchant) {
+            this.merchant = merchant;
+            return this;
+        }
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+        public Builder purchaseDate(LocalDate purchaseDate){
+            this.purchaseDate = purchaseDate;
+            return this;
+        }
+        public Builder amount(double amount) {
+            this.amount = amount;
+            return this;
+        }
+        public Expense build(){
+            return new Expense(this);
+        }
+    }
 }
--- src/main/java/com/divyaanadkat/Expensify/entity/Status.java
+++ src/main/java/com/divyaanadkat/Expensify/entity/Status.java
@@ -13,16 +13,56 @@ public class Status {
 
     @Enumerated(value = EnumType.STRING)
     private State state;
-
     private String reviewedBy;
-
     private LocalDate reviewDate;
-
     private String comment;
 
     public enum State {
         IN_REVIEW,
         APPROVED,
         REJECTED
+    }
+
+    // For Hibernate
+    private Status() {}
+
+    // For Builder
+    private Status(Builder builder) {
+        this.state = builder.state;
+        this.reviewedBy = builder.reviewedBy;
+        this.reviewDate = builder.reviewDate;
+        this.comment = builder.comment;
+    }
+
+    // Expose Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private State state;
+        private String reviewedBy;
+        private LocalDate reviewDate;
+        private String comment;
+
+        public Builder state(State state) {
+            this.state = state;
+            return this;
+        }
+        public Builder reviewedBy(String reviewedBy) {
+            this.reviewedBy = reviewedBy;
+            return this;
+        }
+        public Builder reviewDate(LocalDate reviewDate) {
+            this.reviewDate = reviewDate;
+            return this;
+        }
+        public Builder comment(String comment) {
+            this.comment = comment;
+            return this;
+        }
+        public Status build() {
+            return new Status(this);
+        }
     }
 }
